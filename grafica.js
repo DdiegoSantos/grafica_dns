1fe223b49af394c4696006383a0a507e1aeab909
Diego Nunes Santos <130621994+DdiegoSantos@users.noreply.github.com>
2023-11-06 18:23:16 -0300

    Add files via upload

--- grafica.js
+++ grafica.js
@@ -1,21 +1,14 @@
-function tela() {
-    display = window.screen.width;
-    var boxCard = document.getElementsByClassName(".box-card");
-    let fundo_lonas = document.getElementById('fundo_lonas');
-    if (display < 600) {
+var data = new Date();
+var dia = data.getDate();
+var mes = data.getMonth()+1;
 
-        
-        fundo_lonas.style.padding = '10px';
+function mostrarData() {
 
-        
-        boxCard.style.width = '500px'
-
-    }
+    //alert(dia + "/" +(mes+1));
+    pData = document.getElementById('data');
+    pData.innerHTML = dia + "/" + (mes);
 }
 function calcularValorBanner(item) {
-    var data = new Date();
-    var dia = data.getDate();
-    var mes = data.getMonth()+1;
     
     if (dia >= 1 && dia <= 8 ) {
         var ValorFinal = ["R$55,00", "R$75,00", "R$100,00"];
@@ -30,4 +23,44 @@ function calcularValorBanner(item) {
         preco.innerHTML = ValorFinal[item]
     }
 
+}
+
+function tela() {
+    display = window.screen.width;
+    var boxCard = document.getElementsByClassName(".box-card");
+    let fundo_lonas = document.getElementById('fundo_lonas');
+    if (display < 600) {
+
+
+        fundo_lonas.style.background = '#cecece', fundo_lonas.style.padding = '10px';
+
+
+        boxCard.style.width = '500px'
+
+    }
+}
+function temaMes(){
+    var classeNov = document.getElementsByClassName("nov");
+    
+
+    if(mes == 11){
+        body = document.body
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white'
+
+        for ( i = 0 ; i < classeNov.length; i++){
+            classeNov[i].style.backgroundColor = 'black';
+        }
+        
+    }
+    if(mes == 12){
+        body = document.body
+        body.style.backgroundColor = '#800000';
+        body.style.color = 'white'
+
+        for ( i = 0 ; i < classeNov.length; i++){
+            classeNov[i].style.backgroundColor = '#501111';
+        }
+        
+    }
 }
